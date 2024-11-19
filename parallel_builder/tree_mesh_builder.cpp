4de56c926eda5d7928b4a08a4a38d76cd771121a
Explosiver001 <misel-olsany@seznam.cz>
2024-11-19 10:59:11 +0100

    Tree implementation

--- parallel_builder/tree_mesh_builder.cpp
+++ parallel_builder/tree_mesh_builder.cpp
@@ -27,15 +27,87 @@ unsigned TreeMeshBuilder::marchCubes(const ParametricScalarField &field)
     // It is also strongly suggested to first implement Octree as sequential
     // code and only when that works add OpenMP tasks to achieve parallelism.
     
-    return 0;
+    return buildTree(Vec3_t<float>(), field, mGridSize);
+}
+
+
+unsigned TreeMeshBuilder::buildTree(const Vec3_t<float> &cubeOffset, const ParametricScalarField &field, const unsigned gridSize){
+    
+    if(isCubeEmpty(cubeOffset, field, float(gridSize))){
+        return 0;
+    }
+    
+    if(gridSize <= GRID_SIZE_CUT_OFF)
+    {
+        return buildCube(cubeOffset, field);
+    }
+
+    unsigned innerGridSize = gridSize / 2;
+    float innerGridEdge = float(innerGridSize);
+    unsigned totalTriangles = 0;
+
+    size_t innerCubes = sc_vertexNormPos.size();
+
+    for (int i = 0; i < innerCubes; i++){
+        Vec3_t<float> vertexNormPos = sc_vertexNormPos.at(i);
+        Vec3_t<float> innerCubeOffset(
+				cubeOffset.x + vertexNormPos.x * innerGridEdge,
+				cubeOffset.y + vertexNormPos.y * innerGridEdge,
+				cubeOffset.z + vertexNormPos.z * innerGridEdge
+			);
+        totalTriangles += buildTree(innerCubeOffset, field, innerGridSize);
+    }
+    
+    return totalTriangles;
+}
+
+bool TreeMeshBuilder::isCubeEmpty(const Vec3_t<float> &cubeOffset, const ParametricScalarField &field, const float cubeEdgeLength){
+
+    float fullEdgeLength = cubeEdgeLength * mGridResolution;
+	float halfEdgeLength = fullEdgeLength / 2.F;
+    
+	const Vec3_t<float> cubeCenter(
+		cubeOffset.x * mGridResolution + halfEdgeLength,
+		cubeOffset.y * mGridResolution + halfEdgeLength,
+		cubeOffset.z * mGridResolution + halfEdgeLength
+	);
+
+	static const float expr = sqrtf(3.F) / 2.F;
+
+	return evaluateFieldAt(cubeCenter, field) > (mIsoLevel + expr * fullEdgeLength);
 }
 
 float TreeMeshBuilder::evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field)
 {
-    return 0.0f;
+    // NOTE: This method is called from "buildCube(...)"!
+
+    // 1. Store pointer to and number of 3D points in the field
+    //    (to avoid "data()" and "size()" call in the loop).
+    const Vec3_t<float> *pPoints = field.getPoints().data();
+    const unsigned count = unsigned(field.getPoints().size());
+    
+    float value = std::numeric_limits<float>::max();
+
+
+    // 2. Find minimum square distance from points "pos" to any point in the
+    //    field.
+    #pragma omp simd reduction(min:value)
+    for(unsigned i = 0; i < count; ++i)
+    {
+        float distanceSquared  = (pos.x - pPoints[i].x) * (pos.x - pPoints[i].x);
+        distanceSquared       += (pos.y - pPoints[i].y) * (pos.y - pPoints[i].y);
+        distanceSquared       += (pos.z - pPoints[i].z) * (pos.z - pPoints[i].z);
+
+        // Comparing squares instead of real distance to avoid unnecessary
+        // "sqrt"s in the loop.
+        value = std::min(value, distanceSquared);
+    }
+
+    // 3. Finally take square root of the minimal square distance to get the real distance
+    return sqrt(value);
 }
 
 void TreeMeshBuilder::emitTriangle(const BaseMeshBuilder::Triangle_t &triangle)
 {
-    
+    mTriangles.push_back(triangle);
 }
--- parallel_builder/tree_mesh_builder.h
+++ parallel_builder/tree_mesh_builder.h
@@ -13,16 +13,27 @@
 
 #include "base_mesh_builder.h"
 
+
+#define GRID_SIZE_CUT_OFF 1
+
 class TreeMeshBuilder : public BaseMeshBuilder
 {
 public:
     TreeMeshBuilder(unsigned gridEdgeSize);
 
+private:
+    bool isCubeEmpty(const Vec3_t<float> &cubeOffset, const ParametricScalarField &field, const float cubeEdgeLength);
+    unsigned buildTree(const Vec3_t<float> &cubeOffset, const ParametricScalarField &field, const unsigned gridSize);
+
 protected:
     unsigned marchCubes(const ParametricScalarField &field);
     float evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field);
     void emitTriangle(const Triangle_t &triangle);
-    const Triangle_t *getTrianglesArray() const { return nullptr; }
+    const Triangle_t *getTrianglesArray() const { return mTriangles.data(); }
+    
+
+    std::vector<Triangle_t> mTriangles; ///< Temporary array of triangles
+
 };
 
 #endif // TREE_MESH_BUILDER_H
